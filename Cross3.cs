5c94771cef6b9fe6db4eb00f7067f415b37d78c6
Alexander Kalis <alexander.kalis@kukarob.es>
2019-09-04 10:56:40 +0200

    Add project files.

---
+++ Cross3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using PrimaryInterOp.Cross3Krc;
+
+namespace ArchivadoRemoto
+{
+    class Cross3
+    {
+        private static T GetService<T>(string strServiceId, string strClientId)
+        {
+            try
+            {
+                var factory = new KrcServiceFactory();
+
+                var service = factory.GetService(strServiceId, strClientId);
+                if (service == null)
+                {
+                    throw new InvalidOperationException($"No Cross Interface. serviceId \"{strServiceId}\"");
+                }
+                Marshal.ReleaseComObject(factory);
+
+                return (T)service;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.GetType() + ": " + ex.Message);
+            }
+        }
+
+        private static ICKSyncVar2 _syncVar;
+
+        public static ICKSyncVar2 SyncVar => _syncVar ?? (_syncVar = GetService<ICKSyncVar2>("WBC_KrcLib.SyncVar", "TestSyncVar"));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using KukaRoboter.OnlineServicesFacade;
+using System;
+using System.IO;
+
+namespace ArchivadoRemoto
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int entrada = 100;
+            string backupDir = $@"D:\{DateTime.Now:MM/dd/yyyyHH:mm}BackupAll.zip";
+            ArchiveFacade a = new ArchiveFacade();
+
+            while (true)
+                if (Cross3.SyncVar.ShowVar($"$IN[{entrada}]") == "TRUE")
+                {
+                    if (File.Exists(backupDir))
+                    {
+                        File.Delete(backupDir);
+                    }
+                    a.ArchiveAll(backupDir);
+
+                    System.Threading.Thread.Sleep(5000);
+                }
+        }
+    }
+}
+
+
