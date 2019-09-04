9eac5ab571adbe58c81f83c0fa4820a868b890be
Alexander Kalis <alexander.kalis@kukarob.es>
2019-09-04 19:40:16 +0200

    V1.0.4

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace ArchivadoRemoto
         {
             const int entrada = 100;
             const string backupDir = @"D:\BackupAll.zip";
-            string backupDirDt =$@"D:\{DateTime.Now:yyyyMMddhhmm}BackupAll.zip";
             ArchiveFacade a = new ArchiveFacade();
             bool backupDone = false;
             Console.WriteLine($"Esperando entrada {entrada}");
@@ -19,12 +18,14 @@ namespace ArchivadoRemoto
                 if (backupDone)
                 {
                     Console.WriteLine($"Esperando entrada {entrada}");
+                    Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine);
+
                     backupDone = false;
                 }
 
                 if (Cross3.SyncVar.ShowVar($"$IN[{entrada}]") != "TRUE") continue;
                 Console.WriteLine("Haciendo archivado...");
-
+                string backupDirDt = $@"D:\{DateTime.Now:yyyyMMddhhmm}BackupAll.zip";
                 try
                 {
                     if (File.Exists(backupDir)){File.Delete(backupDir);}
